e4f642d80568092caf3137002d2d0b0ad038a712
woow_wu7 <wuxia@shouqianba.com>
2021-08-24 22:58:21 +0800

    feat: todo list4.

--- src/home/home.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable() // 注释，表明是一个provider
-export class HomeService {
-  getHomeService(): string {
-    return 'homeController => homeService';
-  }
-}
